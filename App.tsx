945717ba2fedd5ca1c7291e1e87294ce6f93f2f2
Felipe Duarte <coalajedi.ios@gmail.com>
2023-09-12 10:41:45 -0300

    style: :lipstick: create design system
    This commit will create the design system copied from Figma and implement it at app with ThemeProvider from Styled Component lib

--- App.tsx
+++ App.tsx
@@ -1,6 +1,12 @@
 import React from 'react-native';
 import {Groups} from '@screens/Groups';
+import {ThemeProvider} from 'styled-components';
+import theme from '@theme/index';
 
 export default function App() {
-  return <Groups />;
+  return (
+    <ThemeProvider theme={theme}>
+      <Groups />
+    </ThemeProvider>
+  );
 }
